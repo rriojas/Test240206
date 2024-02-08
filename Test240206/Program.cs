7dfba57f8fabdd1d3484b190a1f3895f1bcbddce
Ruben Riojas <rriojas@gmail.com>
2024-02-08 10:28:53 -0600

    cambio 20240208

--- Test240206/Program.cs
+++ Test240206/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//read a string and print it
+Console.WriteLine("Enter a string: ");
+string str = Console.ReadLine();
+Console.WriteLine("You entered: " + str);
+//read a number and print it
+
+Console.WriteLine("Enter a number: ");
+Int32 num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("You entered: " + num);
+////read a character and print it
+
+Console.WriteLine("Enter a character: ");
+string line = Console.ReadLine();
+if (line.Length==0)
+{
+    Console.WriteLine("You did not enter a character");
+    return;
+}
+
+char ch = line[0];
+Console.WriteLine("You entered: " + ch);
+
 Console.ReadKey();
